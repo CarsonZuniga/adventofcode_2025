e12979e6b95faab59123cd56ffb8e20011b603e0
CarsonZuniga <carsonzuniga@gmail.com>
2025-12-04 14:49:43 -0600

    refactor: consolidate logic into get_max_number function for clarity

--- day3.py
+++ day3.py
@@ -1,34 +1,9 @@
 with open("day3.txt") as f:
     lines = [line.strip() for line in f.readlines()]
 
-### Part 1 ###
-result = 0
-
-for line in lines:
-    max_num = str(line[-2:])
-    for i in range(len(line) - 3, -1, -1):
-        left_digit = max_num[0]
-        right_digit = max_num[1]
-        candidate = line[i]
-        if int(left_digit) >= int(right_digit):
-            candidate_max = f"{candidate}{left_digit}"
-            if int(candidate_max) > int(max_num):
-                max_num = candidate_max
-        elif int(right_digit) > int(left_digit):
-            candidate_max = f"{candidate}{right_digit}"
-            if int(candidate_max) > int(max_num):
-                max_num = candidate_max
-    
-    result += int(max_num)
-
-print(result)
-
-### Part 2 ###
-result = 0
-
-for line in lines:
-    max_num = str(line[-12:])
-    for i in range(len(line) - 13, -1, -1):
+def get_max_number(line, length):
+    max_num = str(line[-length:])
+    for i in range(len(line) - length - 1, -1, -1):
         candidate = line[i]
         batch_max = "0"
         for j in range(len(max_num)-1, -1, -1):
@@ -39,6 +14,21 @@ for line in lines:
         if int(batch_max) > int(max_num):
             max_num = batch_max
 
-    result += int(max_num)
+    return int(max_num)
+
+### Part 1 ###
+result = 0
+
+for line in lines:
+    max_num = get_max_number(line, 2)
+    result += max_num
+
+print(result)
+
+### Part 2 ###
+result = 0
+for line in lines:
+    max_num = get_max_number(line, 12)
+    result += max_num
 
 print(result)
