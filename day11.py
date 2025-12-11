785e89e0202a303d5bfd597acecce94d6c6ad018
CarsonZuniga <carsonzuniga@gmail.com>
2025-12-11 09:45:08 -0600

    finished day11 part 2

--- day11.py
+++ day11.py
@@ -31,3 +31,34 @@ def num_ways_to_out(source):
     return result
 
 print(num_ways_to_out("you"))
+
+### Part 2 ###
+cache: dict[
+    tuple[str, bool, bool],
+    int
+] = {
+    ("out", True, True): 1
+}
+
+def num_ways_to_out_2(source: str, dac_seen: bool, fft_seen: bool):
+    key = (source, dac_seen, fft_seen)
+    if key in cache:
+        return cache[key]
+    elif source == "out":
+        cache[key] = 0
+        return cache[key]
+    
+    result = 0
+    for destination in edges[source]:
+        if destination == "fft":
+            result += num_ways_to_out_2(destination, dac_seen, True)
+        elif destination == "dac":
+            result += num_ways_to_out_2(destination, True, fft_seen)
+        else:
+            result += num_ways_to_out_2(destination, dac_seen, fft_seen)
+
+    cache[key] = result
+
+    return result
+
+print(num_ways_to_out_2("svr", False, False))
